d7b4c79e1a650b2066c1817a4094789010fd6555
timothythomas1 <tuxes-parlay0s@icloud.com>
2023-09-23 14:58:00 -0700

    added git attributes

--- git_lfs_test.py
+++ git_lfs_test.py
@@ -1,8 +1,3 @@
-test = 'test1'
-test = 'test1'
-test = 'test1'
-test = 'test1'
-test = 'test1'
-test = 'test1'
-test = 'test1'
-test = 'test1'
+version https://git-lfs.github.com/spec/v1
+oid sha256:fcc88e4cb626f35be8479682ec79d6748431e9b55c1f0f0dc668f597785ea36a
+size 120
--- git_lfs_test_2.py
+++ git_lfs_test_2.py
@@ -1,8 +1,3 @@
-test = 'test2'
-test = 'test2'
-test = 'test2'
-test = 'test2'
-test = 'test2'
-test = 'test2'
-test = 'test2'
-test = 'test2'
+version https://git-lfs.github.com/spec/v1
+oid sha256:a8f4523d94396a868b2c95d81a04281c53e9c0edfa450b5f7dfa87278afe8498
+size 120
